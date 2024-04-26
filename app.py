92082db6d0b1c9e68281ef24922ccca3e26a65ee
Haroun <jj.haroon99@gmail.com>
2024-04-26 21:21:47 +0300

    add login page

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['name'] = request.form['name']
+        session['age'] = request.form['age']
+        session['language'] = request.form['language']
+        return redirect(url_for('home'))
+    return render_template('login.html')
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+@app.route('/home')
+def home():
+    if 'name' in session:
+        return f"Welcome {session['name']} to the Treasure Hunt!"
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run()
